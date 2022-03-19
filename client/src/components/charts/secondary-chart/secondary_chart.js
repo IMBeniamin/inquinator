936085651e04e47abd4a9dd8ef24edd1a489db8d
I.M <beniiorga@gmail.com>
2022-03-19 15:17:08 +0100

    moved files to separate repositories

--- client/src/components/charts/secondary-chart/secondary_chart.js
+++
@@ -1,9 +0,0 @@
-import React, { memo, useEffect, useState } from "react";
-import { Bar } from "react-chartjs-2";
-
-const SecondaryChart = (props) => {
-  const [dataChart, setDataChart] = useState(Object);
-  return <Bar />;
-};
-
-export default memo(SecondaryChart);
